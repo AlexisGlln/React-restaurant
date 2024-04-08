dfc7dda2cc547f3265ca76e26ad83b3140bb9f9a
alexis.guillon <alexis.guillon44@orange.fr>
2024-04-08 15:40:15 +0200

    Restaurant is open

--- src/components/MenuList.jsx
+++ src/components/MenuList.jsx
@@ -3,22 +3,6 @@ import MenuItem from "./MenuItem";
 
 function MenuList({ foodItems }) {
 
-
-  /* 
-  array.map((toto) => { })
-  
-  1. Function anonyme FUNCTION
-    function () {}
-  2. Function anonyme ARROW
-    () => {}
-
-  1. Function FUNCTION
-    function nomDeFunction() {}
-  2. Function ARROW
-    const nomDeFunction = () => {}
-
-  */
-
   return (
     <>
       { foodItems.map((el) => {
